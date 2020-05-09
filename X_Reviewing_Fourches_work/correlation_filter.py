3994a4638d3a02032700f42d9bb450eb64d72233
Joel <ricci.lob@gmail.com>
2020-05-09 10:44:34 -0700

    Functions to perform feature selection by dropping correlated features

---
+++ X_Reviewing_Fourches_work/correlation_filter.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import numpy as np
+
+def correlation(dataset, threshold):
+    # Creates a dictionary with feature names and a counter initialized at zero
+    counter_of_corrs = {f: 0 for f in dataset.columns}
+    col_corr = set() # Set of all the names of deleted columns
+    corr_matrix = dataset.corr(method='spearman')
+    for i in range(len(corr_matrix.columns)):
+        for j in range(i):
+            if (corr_matrix.iloc[i, j] >= threshold):
+                colname_i = corr_matrix.columns[i] # getting the name of column i
+                colname_j = corr_matrix.columns[j] # getting the name of column j
+                counter_of_corrs[colname_i] += 1
+                counter_of_corrs[colname_j] += 1
+    # Trasnform the result to a series
+    counter_of_corrs = pd.Series(counter_of_corrs)
+    return(counter_of_corrs)
+
+def drop_features(dataset, thr, min_thr, step, list_of_droped_features, verbose = False):
+    counter_of_corrs = correlation(dataset, thr)
+    max_n_correlations = counter_of_corrs.max()
+    if max_n_correlations > 0:
+        # Get the features with the max value
+        features = counter_of_corrs[counter_of_corrs == max_n_correlations]
+        features_names = [i for i in features.index]
+        if verbose: print(features_names)
+        # Drop one feature ramdomly
+        feature_to_drop = np.random.choice(features_names)
+        list_of_droped_features.append(feature_to_drop)
+        if verbose: print('Droped conf:', feature_to_drop, '-> rho =', round(thr, 3))
+        new_dataset = dataset.drop([feature_to_drop], axis = 1)
+        # Recursively
+        drop_features(new_dataset, thr, min_thr, step, list_of_droped_features, verbose)
+    elif thr >= min_thr:
+        #print(thr, min_thr)
+        drop_features(dataset, thr - step, min_thr, step, list_of_droped_features, verbose)
+
+def features_to_drop(dataset, min_thr, max_thr, step, verbose = False):
+    dataset = dataset.copy()
+    list_of_droped_features = []
+    # User recursion to find
+    drop_features(dataset, max_thr, min_thr, step, list_of_droped_features, verbose = verbose)
+    return(list_of_droped_features)
